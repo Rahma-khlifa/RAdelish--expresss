c80e9a492e77537f85e4d4fe4bd20834c467b56f
Rahma-khlifa <rahmmaakhlefa@gmail.com>
2024-05-18 16:19:57 +0100

    final commit

---
+++ app/src/main/java/com/example/foodapp/Domaine/Price.java
@@ -0,0 +1,31 @@
+package com.example.foodapp.Domaine;
+
+public class Price {
+    private int Id;
+    private String  Value;
+
+    @Override
+    public String toString() {
+        return Value;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getValue() {
+        return Value;
+    }
+
+    public void setValue(String value) {
+        Value = value;
+    }
+
+    public Price(){
+
+    }
+}
